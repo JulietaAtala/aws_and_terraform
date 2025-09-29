8f6d28a1111bd4c516e391596a6d25e3bd51a03a
atala89549 <julieta_atala@epam.com>
2025-09-29 09:29:47 -0300

    task 4

---
+++ iam.tf
@@ -0,0 +1,57 @@
+resource "aws_iam_group" "iam_group" {
+  name = "${local.name_for}-iam-group"
+}
+
+resource "aws_iam_policy" "cmtr-m68g13qx-iam-policy" {
+  name        = "${local.name_for}-iam-policy"
+  description = "My example policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "arn:aws:s3:::${local.name_for}-bucket-1759147576"
+      }
+    ]
+  })
+
+  tags = {
+    Project   = local.name_for
+    Terraform = "true"
+  }
+}
+
+resource "aws_iam_role" "iam_role" {
+  name = "${local.name_for}-iam-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+  tags = {
+    Project   = local.name_for
+    Terraform = "true"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "role_attach" {
+  role       = aws_iam_role.iam_role.name
+  policy_arn = aws_iam_policy.cmtr-m68g13qx-iam-policy.arn
+}
+
+resource "aws_iam_instance_profile" "my_instance_profile" {
+  name = "${local.name_for}-iam-instance-profile"
+  role = aws_iam_role.iam_role.name
+}
